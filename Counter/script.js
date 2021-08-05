a5cb6786444fc5835755699d4d4f1dbf9186155f
Dhrumil Surelia <46326109+DhrumilSurelia@users.noreply.github.com>
2021-08-05 18:55:18 +0530

    Update script.js

--- Counter/script.js
+++ Counter/script.js
@@ -1,9 +1,15 @@
 let plusButton = document.getElementById("increment");
+let minusButton = document.getElementById("decrement");
 let text = document.getElementById("Text");
 
 let count = 0;
 
 plusButton.addEventListener('click', () => {
     count ++;
-    text.innerHTML = count;
+    text.innerHTML = count + ": Increment";
+  })
+
+  minusButton.addEventListener('click', () => {
+    count --;
+    text.innerHTML = "Decrement : " + count;
   })
